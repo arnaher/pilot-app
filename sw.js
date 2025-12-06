d90e0e334b80643f20dad0f5f320c61b866919b3
arnaher <arnaudherault@gmail.com>
2025-12-06 22:43:43 +0100

    Add files via upload
    Initial commit

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pilot-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/static/js/bundle.js',
+        '/static/css/main.css'
+      ]).catch((error) => {
+        console.log('Cache addAll error:', error);
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
